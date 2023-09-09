1651bfcc4aa6706146fa6490f9522511d3cc7a13
hebaosman443 <hebaosman443@gmail.com>
2023-09-09 22:16:29 +0300

    add Inital API

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,42 @@
+
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context )
+       {
+            _context = context;
+        }
+            // GET: api/Users
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            return Ok(users);
+        }
+// GET: api/Users/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound(); // Return a 404 Not Found response if the user is not found.
+            }
+
+            return Ok(user);
+        }
+       
+    }
+}
